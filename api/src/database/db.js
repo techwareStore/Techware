9ddf3b15981f25b6862d6f9c23e4b6a57d3c33b8
Rodrigo Román <romanrodri03@gmail.com>
2024-11-01 16:06:28 -0300

    sequelize

--- api/src/database/db.js
+++ api/src/database/db.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
-const { NAME_DATABASE, PASSWORD, USERNAME } = process.env
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env
 
-
-const sequelize = new Sequelize("techware", "postgres", "postgres", {
-    host: 'localhost',
-    dialect: 'postgres',
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`,
+  {
     logging: false,
     native: false,
-});
+  }
+);
 
 
 module.exports = {
